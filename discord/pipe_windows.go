214badfe676ed4b221d5e9bd03b50a4953ed22f3
LuzDivina_s7n <204550826+luzdivina-s7n@users.noreply.github.com>
2026-08-25 19:51:29 +0500

    250826

--- discord/pipe_windows.go
+++ discord/pipe_windows.go
@@ -1,20 +1,22 @@
 //go:build windows
 
 package discord
+
 import (
-	"fmt"
-	"net"
-	"github.com/Microsoft/go-winio"
+    "fmt"
+    "net"
+    "github.com/Microsoft/go-winio"
 )
+
 func dialPipe() (net.Conn, error) {
-	var lastErr error
-	for i := 0; i < 10; i++ {
-		path := fmt.Sprintf(`\\.\pipe\discord-ipc-%d`, i)
-		conn, err := winio.DialPipe(path, nil)
-		if err == nil {
-			return conn, nil
-		}
-		lastErr = err
-	}
-	return nil, fmt.Errorf("cannot connect to discord IPC pipe: %w", lastErr)
+    var lastErr error
+    for i := 0; i < 10; i++ {
+        path := fmt.Sprintf(`\\.\\pipe\\discord-ipc-%d`, i)
+        conn, err := winio.DialPipe(path, nil)
+        if err == nil {
+            return conn, nil
+        }
+        lastErr = err
+    }
+    return nil, fmt.Errorf("cannot connect to discord IPC pipe: %w", lastErr)
 }
